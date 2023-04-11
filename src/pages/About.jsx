93aea6ad7a91d5c73ef79c295e5adcc2a1a57bec
Joshua T J <joshuajose312@gmail.com>
2023-04-11 19:48:46 +0530

    Initial Commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        deserunt enim obcaecati. Rerum sapiente voluptates recusandae error
+        omnis ut explicabo quae nisi in debitis laudantium animi minima quos
+        asperiores, vitae nulla officia autem voluptatem obcaecati delectus
+        consequatur? Temporibus, delectus possimus at repellat fuga provident
+        sit veritatis animi recusandae reiciendis asperiores?
+      </p>
+    </section>
+  );
+};
+
+export default About;
